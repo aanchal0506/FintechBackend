ae9a30c07ce150db2332cd793439314a319764da
aanchal0506 <ladhaanchal@gmail.com>
2026-09-02 10:43:20 +0530

    create account

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -4,7 +4,8 @@ const accountModel = require("../models/account.model");
 async function createAccountController(req, res){
     const user = req.user;
     const account = await accountModel.create({
-        user : user._id
+        user : user._id,
+        currency: req.body.currency
     })
     res.status(201).json({
         account
--- src/models/account.model.js
+++ src/models/account.model.js
@@ -11,8 +11,8 @@ const accountSchema = new mongoose.Schema({
         type: String,
         enum: {
         values: ["ACTIVE", "FROZEN", "CLOSED"],
-        message: "Status can be either ACTIVE, FROZEN or CLOSED"
-        },
+        message: "Status can be either ACTIVE, FROZEN or CLOSED",
+       },
         default: "ACTIVE"
     },
     currency : {
