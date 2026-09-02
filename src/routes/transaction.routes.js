91f5d77b6f4c9dca5287ba887dd715fd60828b5b
aanchal0506 <ladhaanchal@gmail.com>
2026-09-02 23:50:18 +0530

    transaction routes are set

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,15 +1,13 @@
 const {Router} = require('express');
 const authMiddleware = require("../middlewares/auth.middleware")
-
+const transactionController = require('../controllers/transaction.controller')
 
 
 const transactionRoutes = Router();
 
 //POST /api/transactions/
 //create a new transaction
-
-
-transactionRoutes.post("/",authMiddleware.authMiddleware)
+transactionRoutes.post("/",authMiddleware.authMiddleware,transactionController.createTransaction)
 
 
 module.exports = transactionRoutes;
